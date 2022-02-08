8470678684624451e3cf5666d72eb3d3bdec9cc1
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-02-08 09:19:00 +0530

    modified:   sync.sh

--- sync.sh
+++ sync.sh
@@ -4,10 +4,9 @@ MAGMA_ROOT=/Users/shubham/myfiles/magma-production/mini-magma
 MAGMA_GALAXY_ROOT=/Users/shubham/myfiles/git/magma-galaxy
 
 MINI_MAGMA_ROOT=${MAGMA_ROOT}/orc8r/cloud/deploy/mini-magma
-MAGMA_GALAXY_ROLES_ROOT=${MAGMA_GALAXY_ROOT}/roles
 
 # Sync roles
-rsync -a --delete ${MAGMA_GALAXY_ROLES_ROOT} ${MINI_MAGMA_ROOT}
+rsync -a --delete ${MAGMA_GALAXY_ROOT}/roles ${MINI_MAGMA_ROOT}
 
 find ${MINI_MAGMA_ROOT}/roles -type f -name 'README.md' -delete
 find ${MINI_MAGMA_ROOT}/roles -type d -name 'meta' -exec rm -rf {} +
